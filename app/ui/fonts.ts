6331c759d0880f17fafd3e32a4fbe997684013a3
Kimble86 <kimmo.salmi@gmail.com>
2025-08-13 22:45:34 +0800

    Commitments eh?

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+// Import Google Fonts
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'], // Specify desired weights (e.g., normal and bold)
+    subsets: ['latin'], // Required subset for Google Fonts
+});
